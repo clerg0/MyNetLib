ca53ca5fe0300c6abf84188c441d0c39aa580499
Damien Clergeaud <Damien Clergeaud>
2016-10-21 15:02:21 +0200

    second commit

---
+++ Assets/Scripts/NetLib/NetManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class NetManager : MonoBehaviour {
+
+    public bool host;
+
+    public int socketId;
+    public int socketPort = 8888;
+
+    public string IpAddress = "127.0.0.1";
+
+    public int myReliableChannelId;
+
+
+	// Use this for initialization
+	void Start () {
+        if (host)
+        {
+            HostInit();
+        }
+	}
+
+    void HostInit()
+    {
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+        myReliableChannelId = config.AddChannel(QosType.Reliable);
+        int maxConnections = 10;
+        HostTopology topology = new HostTopology(config, maxConnections);
+
+        socketId = NetworkTransport.AddHost(topology, socketPort);
+        Debug.Log("Socket Open. SocketId is: " + socketId);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (host)
+        {
+            ReceiveSocketMessage();
+        }
+	}
+
+    public void ReceiveSocketMessage()
+    {
+        int recHostId;
+        int recConnectedId;
+        int recChannelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostId, out recConnectedId, out recChannelId, recBuffer, bufferSize, out dataSize, out error);
+
+        switch (recNetworkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("incoming connection event received");
+                break;
+            case NetworkEventType.DataEvent:
+                Stream stream = new MemoryStream(recBuffer);
+                BinaryFormatter formatter = new BinaryFormatter();
+                string message = formatter.Deserialize(stream) as string;
+                Debug.Log("incoming message event received: " + message);
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("remote client event disconnected");
+                break;
+        }
+    }
+}
